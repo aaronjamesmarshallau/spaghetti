f0777e2c7ee6094f0908971b3ea80a5fe327d40e
Aaron Marshall <hi@imaaronnicetomeetyou.me>
2020-05-29 20:54:11 +1000

    Adds ability to create recipes

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,5 +1,5 @@
 use rocket_contrib::json::Json;
-use crate::models::recipe::ThinRecipe;
+use crate::models::recipe::{ThinRecipe, Recipe, NewRecipe};
 use crate::db_connection::PostgresConnection;
 use crate::models::Response;
 
@@ -8,7 +8,21 @@ pub fn get_single_recipe(id: i32, connection: PostgresConnection) -> Json<Respon
     let result = ThinRecipe::find(&id, &connection);
 
     if !result.is_ok() {
-        println!("No result returned from get_single_recipe...");
+        println!("No result returned from get_single_recipe: {}", result.err().unwrap());
+        return Json(None.into());
+    }
+
+    Json(result.ok().into())
+}
+
+#[post("/api/recipe", format = "application/json", data = "<raw_recipe>")]
+pub fn create_single_recipe(raw_recipe: Json<NewRecipe>, connection: PostgresConnection) -> Json<Response<ThinRecipe>> {
+    let recipe = raw_recipe.0;
+    let result = Recipe::create(&recipe, &connection);
+
+    if !result.is_ok() {
+        println!("Unable to create new recipe {}: {}", recipe.name, result.err().unwrap());
+        return Json(None.into());
     }
 
     Json(result.ok().into())
--- src/models/recipe.rs
+++ src/models/recipe.rs
@@ -17,3 +17,40 @@ impl ThinRecipe {
         recipe::table.find(id).select((recipe::id, recipe::name, recipe::description)).first(connection)
     }
 }
+
+#[derive(Insertable, Serialize, Deserialize)]
+#[table_name = "recipe"]
+pub struct NewRecipe {
+    pub name: String,
+    pub description: String,
+    pub image_url: String,
+}
+
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct Recipe {
+    pub id: i32,
+    pub name: String,
+    pub description: String,
+    pub image_url: String,
+}
+
+impl From<Recipe> for ThinRecipe {
+    fn from(rcp: Recipe) -> ThinRecipe {
+        ThinRecipe {
+            id: rcp.id,
+            name: rcp.name,
+            description: rcp.description,
+        }
+    }
+}
+
+impl Recipe {
+    pub fn create(rcp: &NewRecipe, connection: &PgConnection) -> Result<ThinRecipe, diesel::result::Error> {
+        use diesel::RunQueryDsl;
+
+        diesel::insert_into(recipe::table)
+            .values(rcp)
+            .get_result::<Recipe>(connection)
+            .map(Into::into)
+    }
+}
